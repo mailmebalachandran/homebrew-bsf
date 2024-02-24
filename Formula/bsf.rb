3577c8773f2a582963ee232533bc576fc0614a47
Geethadevi91 <mailme.geethabalachandran@gmail.com>
2024-02-24 19:38:33 +0800

    adding the rb formula

---
+++ Formula/bsf.rb
@@ -0,0 +1,17 @@
+class Bsf < Formula
+    desc "This tool search the pattern including the sub directories for the given path"
+    homepage "https://github.com/mailmebalachandran/homebrew-bsf"
+    url "https://github.com/mailmebalachandran/bsf/archive/v0.1.0.tar.gz"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    license "MIT OR Apache-2.0"
+  
+    depends_on "rust" => :build
+  
+    def install
+      system "cargo", "install", "--root", prefix, "--path", "."
+    end
+  
+    test do
+      system "#{bin}/bsf", "--version"
+    end
+  end
