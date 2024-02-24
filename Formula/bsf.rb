48d4a660044a93aa2424540760a07cc00cff69a3
Geethadevi91 <mailme.geethabalachandran@gmail.com>
2024-02-24 21:48:12 +0800

    rename of url was done

--- Formula/bsf.rb
+++ Formula/bsf.rb
@@ -3,7 +3,7 @@ class Bsf < Formula
     homepage "https://github.com/mailmebalachandran/homebrew-bsf"
     url "https://github.com/mailmebalachandran/homebrew-bsf/archive/v0.1.0.tar.gz"
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    license "MIT OR Apache-2.0"
+    license "MIT OR Apache-2.0 "
   
     depends_on "rust" => :build
   
